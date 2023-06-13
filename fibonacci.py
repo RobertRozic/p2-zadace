bd4b563535cdef871280fbd893d4ae15acbdbc26
Robert Rozic <r.rozic97@gmail.com>
2023-06-13 13:42:01 +0200

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+# Datoteka fibonacci.py
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+testna = lambda s: s[::-1]
---
+++ moduli1.py
@@ -0,0 +1,12 @@
+import sys
+#print(sys.builtin_module_names)
+
+import fibonacci as f
+
+broj=int(input("Unesi broj"))
+print("test")
+print(f.fib(broj))
+
+print(f.testna("test"))
+
+
